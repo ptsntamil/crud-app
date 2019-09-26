e53be24af9fb794a39473e9e397bc29fb7126423
Tamilarasan Paneerselvam <tamilarasan@ideas2it.com>
2019-09-26 19:52:17 +0530

    Fix

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,7 +1,16 @@
 const FILES_TO_CACHE = [
   '/offline.html',
+  '/404.html',
+  '/index.html',
+  'dist/js/chunk-vendors.1f3fa4c2.js',
+'dist/js/app.89f45199.js',
+'dist/service-worker.js',
+'dist/js/about.c47c471e.js',
+'dist/css/chunk-vendors.e231a253.css',
+'dist/css/app.22b7f024.css'
 ];
-const CACHE_NAME = "CRUD-PWA";
+const CACHE_NAME = "CRUD-PWA_1";
+const DATA_CACHE_NAME = "DATA_CRUD-PWA_1";
 self.addEventListener('install', (evt) => {
   console.log('[ServiceWorker] Install');
   // CODELAB: Precache static resources here.
@@ -20,7 +29,7 @@ self.addEventListener('activate', (evt) => {
   evt.waitUntil(
     caches.keys().then((keyList) => {
       return Promise.all(keyList.map((key) => {
-        if (key !== CACHE_NAME) {
+        if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
           console.log('[ServiceWorker] Removing old cache', key);
           return caches.delete(key);
         }
@@ -31,15 +40,44 @@ self.addEventListener('activate', (evt) => {
 });
 
 self.addEventListener('fetch', (evt) => {
-  console.log('[ServiceWorker] Fetch', evt.request.url);
-  // CODELAB: Add fetch event handler here.
-  if (evt.request.mode !== 'navigate') {
-    // Not a page navigation, bail.
+  // console.log('[ServiceWorker] Fetch', evt.request.url);
+  // // CODELAB: Add fetch event handler here.
+  // if (evt.request.mode !== 'navigate') {
+  //   // Not a page navigation, bail.
+  //   return;
+  // }
+  // evt.respondWith(fetch(evt.request).catch(_ => {
+  //   return caches.open(CACHE_NAME).then(cache => {
+  //     return cache.match('offline.html');
+  //   });
+  // }));
+  if (evt.request.url.includes('reqres')) {
+    console.log('[Service Worker] Fetch (data)', evt.request.url);
+    evt.respondWith(
+        caches.open(DATA_CACHE_NAME).then((cache) => {
+            console.log("DATACACHE", cache);
+          return fetch(evt.request)
+              .then((response) => {
+                // If the response was good, clone it and store it in the cache.
+                if (response.status === 200) {
+                  cache.put(evt.request.url, response.clone());
+                }
+                return response;
+              }).catch((err) => {
+                // Network request failed, try to get it from the cache.
+                return cache.match(evt.request);
+              });
+        }));
     return;
   }
-  evt.respondWith(fetch(evt.request).catch(_ => {
-    return caches.open(CACHE_NAME).then(cache => {
-      return cache.match('offline.html');
-    });
-  }));
+  evt.respondWith(
+      caches.open(CACHE_NAME).then((cache) => {
+        return cache.match(evt.request)
+            .then((response) => {
+              console.log("REQ",evt.request);
+              console.log("res", response);
+              return response || fetch(evt.request);
+            });
+      })
+  );
 })
